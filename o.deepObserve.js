ce5fcc1318cd49d67aebe75f830d848cbd9a4288
Jiří Špác <capajj@gmail.com>
2014-08-26 21:27:02 +0200

    first two unit tests

--- o.deepObserve.js
+++ o.deepObserve.js
@@ -10,8 +10,14 @@ Object.defineProperty(Object, "isObject", {
 Object.defineProperty(Object, "deepObserve", {
 	enumerable: false,
 	writable: true,
+    /**
+     * observes changes on tree of objects
+     * @param {Object} obj
+     * @param {Function} CB
+     */
 	value: function deepObserve(obj, CB, notifier, previousPath) {
-		var notf = Object.getNotifier(obj);
+
+        var notf = notifier || Object.getNotifier(obj);
 		var keys = Object.keys(obj);
 		if (!previousPath) {
 			previousPath = [];
@@ -30,33 +36,39 @@ Object.defineProperty(Object, "deepObserve", {
 		}
 
 		Object.observe(obj, function(changes) {
-			var i = changes.length;
+            console.log("changes triggered " + changes.length );
+            var i = changes.length;
 			while (i--) {
 				var change = changes[i];
 
 			//  console.log(change.type, change.name, change.oldValue);
 				if (change.type === 'add') {
-					var addedObj = change.object[change.name];
-					if (isObject(addedObj)) {
+                    console.log("add");
+                    var addedObj = change.object[change.name];
+					if (Object.isObject(addedObj)) {
 						console.log("Observing new property because it is an object " + change.name);
 						recourse(addedObj, change.name);
 					}
 				} else if (change.type === 'delete') {
-					console.log("delete");
+					//console.log("delete");
 				} else if (change.type === 'update') {
 					if (typeof change.oldValue === 'object') {
 
 					}
 					var changed = change.object[change.name];
-					if (isObject(changed)) {
+					if (Object.isObject(changed)) {
 						recourse(changed, change.name);
 					}
-					console.log("update");
+					//console.log("update");
 				}
 			}
 			if (!notifier) {
 				CB.apply(obj, arguments);
-			}
+			} else {
+                change.path = previousPath.join('.') + change.name;
+                console.log("deep notif");
+                notf('deep' + change.type, change);
+            }
 		});
 
 	}
--- test/test.js
+++ test/test.js
@@ -1,13 +1,58 @@
 describe('deepObserve', function() {
 	var observed;
+	var observer;
+    var changes;
 
 	beforeEach(function() {
 		observed = {};
+        changes = [];
+        Object.deepObserve(observed, function (changesTriggered) {
+            changes.push(changesTriggered);
+        });
 	});
 
-	it("should be able to watch simple object and it's values same as regular Object.observe", function() {
-		var changes = [];
-		//pending();
-		expect(true).toBe(true);
+	xit("should be able to watch simple object and it's values same as regular Object.observe", function() {
+
+        runs(function () {
+            observed.a = 1;
+            observed.a = 2;
+            delete observed.a;
+            setTimeout(function () {
+                observed.b = 0;
+            }, 4)
+        });
+
+        waits(10);
+
+        runs(function () {
+
+            expect(changes.length).toBe(2);
+            expect(changes[0].length).toBe(3);
+            expect(changes[0][0].type).toBe('add');
+
+        });
 	});
+
+    it('should be able to watch an object assigned to the root object', function () {
+        runs(function () {
+            var another={};
+            observed.a = another;
+            //delete observed.a;
+            setTimeout(function () {
+                another.a = 2;
+
+                observed.b = 0;
+            }, 4)
+        });
+
+        waits(10);
+
+        runs(function () {
+
+            expect(changes.length).toBe(2);
+            expect(changes[0].length).toBe(3);
+            expect(changes[0][0].type).toBe('add');
+
+        });
+    })
 });
