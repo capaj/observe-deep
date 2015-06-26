7cefe41d7ebdac590ca2021ae130b702f537f531
Jiří Špác <capajj@gmail.com>
2015-06-26 16:41:17 +0200

    editorconfig

--- test/test.js
+++ test/test.js
@@ -1,66 +1,73 @@
 describe('deepObserve', function() {
-	var observed;
-	function observer(changesTriggered) {
-        changes.push(changesTriggered);
-    }
-    var changes;
-    var deliver;
+  var observed;
 
-	beforeEach(function() {
-		observed = {};
-        changes = [];
-        deliver = Object.deepObserve(observed, observer);
-	});
+  function observer(changesTriggered) {
+    changes.push(changesTriggered);
+  }
 
-	xit("should be able to watch simple object and it's values same as regular Object.observe, deliver function should be returned", function(done) {
+  var changes;
+  var deliver;
 
-		observed.a = 1;
-		observed.a = 2;
-		delete observed.a;
-        deliver();
-        expect(changes.length).toBe(1);
+  beforeEach(function() {
+    observed = {};
+    changes = [];
+    deliver = Object.deepObserve(observed, observer);
+  });
+  
+  it('should throw when cyclic object is detected', function(){
+      //TODO implement
+  });
 
-        setTimeout(function () {
-			observed.b = 0;
-		}, 4);
+  xit("should be able to watch simple object and it's values same as regular Object.observe, deliver function should be returned", function(done) {
 
-		setTimeout(function(){
-			expect(changes.length).toBe(2);
-			expect(changes[0].length).toBe(3);
-			expect(changes[0][0].type).toBe('add');
-			done();
-		}, 10);
+    observed.a = 1;
+    observed.a = 2;
+    delete observed.a;
+    deliver();
+    expect(changes.length).toBe(1);
 
-	});
+    setTimeout(function() {
+      observed.b = 0;
+    }, 4);
 
-    it('should watch an object assigned to the root object and stop watching it when it is deleted', function (done) {
+    setTimeout(function() {
+      expect(changes.length).toBe(2);
+      expect(changes[0].length).toBe(3);
+      expect(changes[0][0].type).toBe('add');
+      done();
+    }, 10);
 
-		var another={};
-		observed.a = another;
-		//delete observed.a;
-		setTimeout(function () {
-			another.propOnAnother = 'value on another';
-			observed.b = 0;
-		}, 4);
+  });
 
-		setTimeout(function(){
-			expect(changes.length).toBe(2);
-			expect(changes[0].length).toBe(1);
-			expect(changes[0][0].type).toBe('add');
-			delete observed.a;
-			deliver();
+  it('should watch an object assigned to the root object and stop watching it when it is deleted', function(done) {
 
-		}, 10);
+    var another = {};
+    observed.a = another;
+    //delete observed.a;
+    setTimeout(function() {
+      another.propOnAnother = 'value on another';
+      observed.b = 0;
+    }, 4);
 
-		setTimeout(function(){
-			another.c = 1;
-		}, 12);
+    setTimeout(function() {
+      expect(changes.length).toBe(2);
+      expect(changes[0].length).toBe(1);
+      expect(changes[0][0].type).toBe('add');
+      delete observed.a;
+      deliver();
 
-        setTimeout(function () {
-            expect(changes.length).toBe(2);	//TODO find out reason for failure
+    }, 10);
 
-            done();
-        }, 15)
+    setTimeout(function() {
+      another.c = 1;
+    }, 12);
 
-    })
+    setTimeout(function() {
+      expect(changes.length).toBe(2);	//TODO find out reason for failure
+
+      done();
+    }, 15)
+
+  })
+
 });
